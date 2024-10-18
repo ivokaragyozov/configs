dff053936e65f8c62d41573f317756090a6770b6
Ivo <ivokaragyozov2001@gmail.com>
2024-10-18 05:24:53 +0300

    fix g++20

--- fish/functions/g++20.fish
+++ fish/functions/g++20.fish
@@ -1,4 +1,4 @@
-function g++20 --wraps='g++ -std=c++20 -o _compiled' --wraps='g++ -std=c++20 -o $argv%' --wraps=g++\ -std=c++20\ -o\ \(string\ replace\ -r\ \'\\.cpp\'\ \'\'\ \) --description alias\ g++20=g++\ -std=c++20\ -o\ \(string\ replace\ -r\ \'\\.cpp\'\ \'\'\ \)
-  g++ -std=c++20 -o (string replace -r '\.cpp' '' ) $argv
-        
+function g++20
+set name (string replace -r '\.cpp$' '' $argv[1])
+g++ $argv[1] -o $name -std=c++20
 end
