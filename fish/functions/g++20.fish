514751b97bab8ba542bcc7e39b5494c4563c5d71
Ivo <ivokaragyozov2001@gmail.com>
2024-10-20 22:56:17 +0200

    g++20 to put .out extension

--- fish/functions/g++20.fish
+++ fish/functions/g++20.fish
@@ -1,4 +1,4 @@
 function g++20
-    set name (string replace -r '\.cpp$' '' $argv[1])
+    set name (string replace -r '\.cpp$' '.out' $argv[1])
     g++ $argv[1] -o $name -std=c++20 -O2 -fsanitize=address -Wall -Wextra -Wshadow
 end
