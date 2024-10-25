399faa388508fd4570976f1677e7f82fcc9c6a58
Ivo <ivokaragyozov2001@gmail.com>
2024-10-25 13:50:23 +0200

    g++20 to have access to coding-library

--- fish/functions/g++20.fish
+++ fish/functions/g++20.fish
@@ -1,4 +1,4 @@
 function g++20
     set name (string replace -r '\.cpp$' '.out' $argv[1])
-    g++ $argv[1] -o $name -std=c++20 -O2 -fsanitize=address -Wall -Wextra -Wshadow
+    g++ $argv[1] -o $name -std=c++20 -O2 -fsanitize=address -Wall -Wextra -Wshadow -I/Users/ivokaragyozov/Documents/cp/coding-library
 end
